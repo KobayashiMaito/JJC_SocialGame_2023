5ae1a7a84489f9c4d84a94cb0249d680c88717d6
jjc-kuwabara <kuwabara.ryosuke@nsg.gr.jp>
2023-10-30 12:15:03 +0900

    Battle用サンプルを追加

---
+++ Assets/Scripts/Battle/BattleRenderer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleRenderer : MonoBehaviour
+{
+    public int charaId;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string imagePath = Application.fixDataManager.GetCharaImagePath(charaId);
+        transform.Find("Sprite").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(imagePath);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
